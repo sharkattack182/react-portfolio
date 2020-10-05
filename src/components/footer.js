a56f21f7cc3d1ecb48da9cd8d7dd1cba8037ed9a
Matthew Courtney <matthewdc182@gmail.com>
2020-10-05 00:20:59 -0400

    added in header and footer

---
+++ src/components/footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Footer() {
+    return (
+        <footer class="footer mt-auto py-3 bg-dark ">
+        <div class="container-fluid text-center">
+            <span class="text-muted">Matthew Courtney UNCC Bootcamp 2020</span>
+        </div>
+    </footer>
+    )
+}
+
+export default Footer;
---
+++ src/components/header.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+function Header() {
+    return (
+        <header>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <a className="navbar-brand" href="#">Matthew Courtney</a>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
+                aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div className="navbar-nav">
+                    <a className="nav-item nav-link" href="contact.html">Contact</a>
+                    <a className="nav-item nav-link" href="portfolio.html">Portfolio</a>
+                    <a className="nav-item nav-link active" href="index.html">About</a>
+                </div>
+            </div>
+        </nav>
+    </header>
+    )
+}
+
+export default Header;
